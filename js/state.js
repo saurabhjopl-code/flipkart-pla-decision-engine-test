214a96145212e3371d7b9c1e4b613c91d107449a
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-27 12:30:49 +0530

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,23 @@
-// Global state object
+// Global Application State
+
+export const STATE = {
+    data: {
+        gmvDaily: [],
+        ctrDaily: [],
+        adsDaily: [],
+        campaign: [],
+        keyword: [],
+        dateMaster: []
+    },
+
+    filters: {
+        account: "ALL",
+        startDate: null,
+        endDate: null,
+        month: "ALL",
+        week: "ALL"
+    },
+
+    currentPage: "executive",
+    isLoaded: false
+};
