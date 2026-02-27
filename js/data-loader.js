c19783e46e86a9a90d288ebfda784edcfd657804
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-27 12:58:36 +0530

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -9,18 +9,29 @@ async function fetchCSV(url) {
 
     return rows.map(r => {
         const obj = {};
-        headers.forEach((h, i) => obj[h.trim()] = r[i]);
+        headers.forEach((h, i) => {
+            obj[h.trim()] = r[i];
+        });
         return obj;
     });
 }
 
 export async function loadAllData() {
 
-    STATE.raw.gmvDaily = await fetchCSV("GMV_URL");
-    STATE.raw.ctrDaily = await fetchCSV("CTR_URL");
-    STATE.raw.adsDaily = await fetchCSV("ADS_URL");
-    STATE.raw.campaign = await fetchCSV("CAMPAIGN_URL");
-    STATE.raw.keyword = await fetchCSV("KEYWORD_URL");
+    STATE.raw.gmvDaily = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=126143366&single=true&output=csv");
 
-    STATE.data = { ...STATE.raw };
+    STATE.raw.ctrDaily = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1082219598&single=true&output=csv");
+
+    STATE.raw.adsDaily = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1300284124&single=true&output=csv");
+
+    STATE.raw.campaign = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=2101825819&single=true&output=csv");
+
+    STATE.raw.keyword = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1571425410&single=true&output=csv");
+
+    STATE.raw.dateMaster = await fetchCSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=2129061992&single=true&output=csv");
+
+    // Default copy into working data
+    STATE.data = {
+        ...STATE.raw
+    };
 }
