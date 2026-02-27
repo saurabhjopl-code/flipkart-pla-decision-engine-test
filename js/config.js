eab1a90b019733230e2b28522b3f6061a42e27bd
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-27 12:30:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,12 @@
-// Config settings
+// Google Sheet CSV Endpoints
+
+export const CONFIG = {
+    SHEETS: {
+        GMV_DAILY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=126143366&single=true&output=csv",
+        CTR_DAILY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1082219598&single=true&output=csv",
+        ADS_DAILY: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1300284124&single=true&output=csv",
+        CAMPAIGN: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=2101825819&single=true&output=csv",
+        KEYWORD: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=1571425410&single=true&output=csv",
+        DATE_MASTER: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTd_uFyNN_LsA0hZ4EuRYflMnzY-NwSAn9sRhvPbbeRrRkAe5d07tIEJ_gilGwvR5-H1l3jjTOdjq6j/pub?gid=2129061992&single=true&output=csv"
+    }
+};
